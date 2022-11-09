0b517cd1669237ae914ce603972acb09771211be
tej0897 <tej0897@gmail.com>
2022-11-09 15:08:56 +0530

    AngularMaterialCode

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  productForm !: FormGroup;
+
+}
